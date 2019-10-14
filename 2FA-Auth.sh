c5752536689ebd0d6d406281f67b07ac7b63874e
Vinicius de Alencar <54734939+alencc1986@users.noreply.github.com>
2019-10-14 12:28:38 -0300

    2FA-Auth v1.4-1 (Backup method was modified to ask if you want to include exported tokens)

--- 2FA-Auth.sh
+++ 2FA-Auth.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 
-################################################################################
-#                                                                              #
-#    2FA-Auth // Generating '2FA' codes in your terminal                       #
-#    Copyright (C) 2019  Vinicius de Alencar                                   #
-#                                                                              #
-#    This program is free software: you can redistribute it and/or modify      #
-#    it under the terms of the GNU General Public License as published by      #
-#    the Free Software Foundation, either version 3 of the License, or         #
-#    (at your option) any later version.                                       #
-#                                                                              #
-#    This program is distributed in the hope that it will be useful,           #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#    GNU General Public License for more details.                              #
-#                                                                              #
-#    You should have received a copy of the GNU General Public License         #
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
-#                                                                              #
-################################################################################
+#  2FA-Auth // Generating '2FA' codes in your terminal
+#  Copyright (C) 2019  Vinicius de Alencar
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-Version="v1.4-0"
+Version="v1.4-1"
 
 ProjectDir=".config/2fa-auth"
 TokenDir="$HOME/$ProjectDir/token"
